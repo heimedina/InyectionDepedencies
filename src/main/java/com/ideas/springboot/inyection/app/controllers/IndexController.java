db46c329c91e5bab2c32351257a4ead238139a6b
heimedina <heidimedina23@gmail.com>
2025-04-23 12:25:51 +0200

    commit changes

--- src/main/java/com/ideas/springboot/inyection/app/controllers/IndexController.java
+++ src/main/java/com/ideas/springboot/inyection/app/controllers/IndexController.java
@@ -1,13 +1,21 @@
 package com.ideas.springboot.inyection.app.controllers;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+
+import com.ideas.springboot.inyection.app.services.IServicio;
 
 @Controller
 public class IndexController {
 	
+	@Autowired
+	private IServicio servicio;
+	
 	@GetMapping({"/", "", "/index"})
-	public String index() {
+	public String index(Model model) {
+		model.addAttribute("objeto", servicio.operacion());
 		return "index";
 	}
 
